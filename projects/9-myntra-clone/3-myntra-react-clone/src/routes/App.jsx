f4f9dc0979763f29bb1fcc016278dfc553e8086c
kushalkrd92 <kushalkrd92@gmail.com>
2024-12-12 17:27:13 +0530

    new files added

---
+++ projects/9-myntra-clone/3-myntra-react-clone/src/routes/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+import { Outlet } from "react-router-dom"
+
+function App() {
+  return <>
+    <Header />
+    <Outlet />
+    <Footer />
+  </>
+}
+
+export default App
